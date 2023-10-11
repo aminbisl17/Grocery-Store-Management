4fdc6a14aaa1c6fd4673eb5b5bc4b24405e74c8b
aminbisl17 <amin.b05@icloud.com>
2023-10-11 17:25:20 +0200

    changes v12.1

--- src/tableData.java
+++ src/tableData.java
@@ -1,7 +1,7 @@
 
 
 class tableData {
-    public Object[][] data = {
+     Object[][] data = {
         {1, "Fanta", 14, 1.00f},
         {2, "Schweppess", 10, 0.70f},
         {3, "CocaCola", 4, 0.70f}};
